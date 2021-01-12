5d6e76431ddd3d566958313bc9ec5510b199ba67
KristO <khind120@gmail.com>
2021-01-12 05:22:31 -0500

    tes

--- CharacterCards1.0/src/me/r0m3x/iccards/listeners/sneakListener.java
+++ CharacterCards1.0/src/me/r0m3x/iccards/listeners/sneakListener.java
@@ -1,5 +1,7 @@
 package me.r0m3x.iccards.listeners;
 
+import java.util.concurrent.TimeUnit;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,16 +11,13 @@ import me.r0m3x.iccards.API;
 
 public class sneakListener implements Listener {
 	@EventHandler
-	public void onToggleSneak(PlayerToggleSneakEvent e) {
+	public void PlayerToggleSneakEvent(PlayerToggleSneakEvent e) {
+		
 		Player p = e.getPlayer();
 		if(API.getProfession(p).equalsIgnoreCase("Thief")) {
 			if (e.isSneaking()) {
 		        p.setWalkSpeed(0.6F);
 		    }else {
-		    	p.setWalkSpeed(0.6F);
-		    	p.setWalkSpeed(0.5F);
-		    	p.setWalkSpeed(0.4F);
-		    	p.setWalkSpeed(0.3F);
 		    	p.setWalkSpeed(0.2F);
 		    }
 		}
